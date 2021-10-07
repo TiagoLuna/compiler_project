8faf59884438c98defc37bdbd7049e375fe4f4d5
TiagoLuna <tigo.luna@gmail.com>
2021-10-06 23:57:59 -0300

    change in main.py

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ lexer = lex.lex()
 
 with open(sys.argv[1], 'r') as f:
     lexer.input(f.read())
+    tok_array = [[tok.type, tok.value, tok.lexpos, tok.lineno] for tok in lexer]
 
-    for tok in lexer:
-        tok_array = numpy.array([tok.type, tok.value, tok.lexpos, tok.lineno])
-        print(tabulate([tok_array], headers=['Tipo','Valor','Posição','Linha']),'\n')
-        
+    '''for tok in lexer:
+        tok_array.append([tok.type, tok.value, tok.lexpos, tok.lineno])
     '''
-    for i in tok_array
-    print(tabulate([[tok.type,tok.value]]))
-    '''
+
+    print(tabulate(tok_array, headers=['Tipo','Valor','Posição','Linha']),'\n')
